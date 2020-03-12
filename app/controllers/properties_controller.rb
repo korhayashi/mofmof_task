e01459e6979f2b7a854382d54bfda31245c68a68
kaori hayashi <suzu_33@hotmail.com>
2020-03-12 21:23:13 +0900

    物件新規投稿画面作成

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,17 +1,44 @@
 class PropertiesController < ApplicationController
+  before_action :set_property, only: [:show, :edit, :update, :destroy]
+
   def new
     @property = Property.new
     @property.nearest_stations.build
   end
 
   def create
-    @property = Property.new
+    @property = Property.new(property_params)
     if @property.save
       redirect_to properties_path
     end
   end
 
+  def index
+    @properties = Property.all
+  end
+
+  def show
+    @nearest_stations = @property.nearest_stations
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+    @property.destroy
+    redirect_to properties_path
+  end
+
   private
+    def set_property
+      @property = Property.find(params[:id])
+    end
+
     def property_params
       params.require(:property).permit(
         :property_name,
@@ -19,7 +46,7 @@ class PropertiesController < ApplicationController
         :address,
         :age,
         :note,
-        nearest_stations_attributes: []#: [:route_name, :station_name, :time]
+        nearest_stations_attributes: {}#: [:route_name, :station_name, :time]
       )
     end
 end
--- app/models/property.rb
+++ app/models/property.rb
@@ -12,7 +12,7 @@
 #  updated_at    :datetime         not null
 #
 class Property < ApplicationRecord
-  has_many :nearest_stations
+  has_many :nearest_stations, dependent: :destroy
 
-  accepts_nested_attributes_for :nearest_stations
+  accepts_nested_attributes_for :nearest_stations, reject_if: :all_blank
 end
